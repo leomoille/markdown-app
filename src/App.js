53ef1143e95477f95069d645dfa0456890a66a7b
Léo Moille <leomoille@outlook.com>
2020-10-19 15:49:14 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import marked from 'marked'
+import './App.css'
+
+import { sampleText } from './sampleText'
+
+class App extends Component {
+  state = {
+    text: sampleText
+  }
+
+  componentDidMount() {
+    const text = localStorage.getItem('text')
+    if (text) {
+      this.setState({ text })
+    } else {
+      this.setState({ text: sampleText })
+    }
+  }
+
+  componentDidUpdate() {
+    const { text } = this.state
+    localStorage.setItem('text', text)
+  }
+
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
+
+  renderText = text => {
+    const __html = marked(text, { sanitize: true })
+    return { __html }
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-6'>
+            <textarea
+              onChange={this.handleChange}
+              className='form-control'
+              rows='35'
+              value={this.state.text}
+            ></textarea>
+          </div>
+          <div className='col-sm-6'>
+            <div
+              dangerouslySetInnerHTML={this.renderText(this.state.text)}
+            ></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
